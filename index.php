b6ca42827f541667d70ee79d1a86c679c7afad77
hikaRU-135 <hikari.desu13@gmail.com>
2023-06-02 16:57:15 +0400

    query select where

--- index.php
+++ index.php
@@ -21,11 +21,66 @@ require 'vendor/query.php';
             <h3>Request record from table users</h3>
         </div>
         <div class="content">
-            all records
+            <strong style="font-size: 20px">records</strong>
             <?php
-                echo $data[0]["name"] . "<br/>";
-                echo $data[1]["name"] . " is " . $data[1]["age"] . " years old <br/>";
-                echo $data[2]["name"] . " is " . $data[2]["age"] . " years old and he has" . $data[2]["salary"] . " salary<br/>";
+            echo "<br/>" . $data[0]["name"] . "<br/>";
+            echo $data[1]["name"] . " is " . $data[1]["age"] . " years old <br/>";
+            echo $data[2]["name"] . " is " . $data[2]["age"] . " years old and he has " . $data[2]["salary"] . " salary<br/>";
+            ?>
+        </div>
+        <div class="content">
+            <strong style="font-size: 20px">user with id 3</strong>
+            <?php
+            echo $user_id3["name"];
+            ?>
+        </div>
+        <div class="content">
+            <strong style="font-size: 20px">users with salary 900</strong>
+            <?php
+
+            foreach ($users_salary900 as $one) {
+                echo $one["name"] . '  ';
+            };
+            ?>
+        </div>
+        <div class="content">
+            <strong style="font-size: 20px">users has salary more 400 </strong>
+            <?php
+
+            foreach ($users_salary400  as $one) {
+                echo $one["name"] . '  ';
+            };
+            ?>
+        </div>
+
+
+        <div class="content">
+            <strong style="font-size: 20px">users has salary more or equal 500 </strong>
+            <?php
+
+            foreach ($users_salary500  as $one) {
+                echo $one["name"] . '  ';
+            };
+            ?>
+        </div>
+
+        <div class="content">
+            <strong style="font-size: 20px">users has salary not equal 500 </strong>
+            <?php
+
+            foreach ($users_salaryNOT500  as $one) {
+                echo $one["name"] . '  ';
+            };
+            ?>
+        </div>
+
+        <div class="content">
+            <strong style="font-size: 20px">users has salary equal or a few 500 </strong>
+            <?php
+
+            foreach ($users_salaryFEW500  as $one) {
+                echo $one["name"] . '  ';
+            };
             ?>
         </div>
     </div>
--- vendor/query.php
+++ vendor/query.php
@@ -5,6 +5,36 @@ require_once 'connection.php';
 $query = 'SELECT * FROM users';
 $res = mysqli_query($link, $query) or die(mysqli_error($link));
 
-for ($data = []; $row = mysqli_fetch_assoc($res); $data[] = $row);
+//query id = 3
+$user_id3 = mysqli_fetch_assoc(mysqli_query($link, 'SELECT * FROM users WHERE id=3'));
+
+//query users where salary=900
+$quer_users_900 = mysqli_query($link, 'SELECT * FROM users WHERE salary=900');
+for ($users_salary900 = []; $row = mysqli_fetch_assoc($quer_users_900); $users_salary900[] = $row) ;
+
+//query users age=23
+$quer_users_age23 = mysqli_query($link, 'SELECT * FROM users WHERE age=23');
+for ($users_age23 = []; $row = mysqli_fetch_assoc($quer_users_age23); $users_age23[] = $row) ;
+
+//query users salary>400
+$quer_users_salary400 = mysqli_query($link, 'SELECT * FROM users WHERE salary>400');
+for ($users_salary400 = []; $row = mysqli_fetch_assoc($quer_users_salary400); $users_salary400[] = $row) ;
+
+
+//query users salary>=500
+$quer_users_salary500 = mysqli_query($link, 'SELECT * FROM users WHERE salary>=500');
+for ($users_salary500 = []; $row = mysqli_fetch_assoc($quer_users_salary500); $users_salary500[] = $row) ;
+
+//query users salary!=500
+$quer_users_salaryNOT500 = mysqli_query($link, 'SELECT * FROM users WHERE salary!=500');
+for ($users_salaryNOT500 = []; $row = mysqli_fetch_assoc($quer_users_salaryNOT500); $users_salaryNOT500[] = $row) ;
+
+//query users salary<=500
+$quer_users_salaryFEW500 = mysqli_query($link, 'SELECT * FROM users WHERE salary<=500');
+for ($users_salaryFEW500 = []; $row = mysqli_fetch_assoc($quer_users_salaryFEW500); $users_salaryFEW500[] = $row) ;
+
+
+
+for ($data = []; $row = mysqli_fetch_assoc($res); $data[] = $row) ;
 
 
